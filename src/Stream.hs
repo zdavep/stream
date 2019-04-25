6d34f21cc67e4b3e421a643718be23247e84aae3
Dave Pederson <dave.pederson@gmail.com>
2019-04-24 21:42:47 -0600

    comments

--- src/Stream.hs
+++ src/Stream.hs
@@ -11,13 +11,16 @@ module Stream
 import Control.DeepSeq
 import Control.Monad.Par
 
+-- IList is a parallel data structure.
 data IList a
   = Nil
   | Cons a (IVar (IList a))
-  | Fork (Par ()) (IList a)
+  | Fork (Par ()) (IList a) -- Indicates consumer must `fork` to continue
 
+-- Stream is an IList that can be read (blocking) and written in parallel.
 type Stream a = IVar (IList a)
 
+-- Describe how to fully evaluate an IList
 instance NFData a => NFData (IList a) where
   rnf Nil = ()
   rnf (Cons a b) = rnf a `seq` rnf b
