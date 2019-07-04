fca06efacc05acfdc36f4a1a8a6480920843fd04
Dave Pederson <dave.pederson@gmail.com>
2019-07-04 09:41:57 -0600

    Add alt fib functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,43 @@
 module Main where
 
-import Stream
 import Control.Monad.Par
+import Stream
 
--- Calculate the n-th Fibonacci number
+-- Calculate the n-th Fibonacci number using 0 to handle negatives.
 fib :: Integer -> Integer
-fib 0 = 1
-fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
+fib n
+  | n <= 0 = 0
+  | n == 1 = 1
+  | otherwise = fib (n - 1) + fib (n - 2)
+
+-- Calculate the n-th Fibonacci number using Maybe to handle negatives.
+mfib :: Integer -> Maybe Integer
+mfib n
+  | n < 0 = Nothing
+  | otherwise = Just (fib n)
+
+-- A type for errors
+newtype Error = Error String
+  deriving Show
+
+-- Calculate the n-th Fibonacci number using Either to handle negatives.
+efib :: Integer -> Either Error Integer
+efib n
+  | n < 0 = Left $ Error "n cannot be negative"
+  | otherwise = Right (fib n)
 
 -- Use streams to calculate the sum of even fibonacci numbers.
 pipeline :: Int -> [Integer] -> Integer
 pipeline n xs =
   runPar $ do
-    s0 <- streamFromList n xs              -- emit list in batches of size `n`
+    s0 <- streamFromList n xs -- emit list in batches of size `n`
     s1 <- streamFilter even (n `div` 2) s0 -- emit even numbers + halve batch size
-    s2 <- streamMap fib n s1               -- calc fib number
-    sm <- streamFold (+) 0 s2              -- streaming sum op
-    return sm                              -- result
+    s2 <- streamMap fib n s1 -- calc fib number
+    sm <- streamFold (+) 0 s2 -- streaming sum op
+    return sm -- result
 
--- Calculate and print the sum of some even Fibonacci numbers.
+      -- Calculate and print the sum of some even Fibonacci numbers.
+
 main :: IO ()
 main = do
-  putStrLn $ show (pipeline 10 [1..36])
+  putStrLn $ show (pipeline 10 [1.. 36])
